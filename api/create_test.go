918323f5919ceb417cac83e723e7a91a0f9cbb9e
stjukrop <julian.kropp@stud.h-da.de>
2023-01-02 19:29:55 +0100

    :sparkles: Add CreateMeeting

---
+++ api/create_test.go
@@ -0,0 +1,10 @@
+package api
+
+import "testing"
+
+// Thes tests only succeed if there is a BBB server running, which does not have any other meetings running!!!
+
+// Test for GetMeetings
+func TestCreateMeeting(t *testing.T) {
+	//TODO
+}
--- example.go
+++ example.go
@@ -10,43 +10,50 @@ import (
 )
 
 type config struct {
-	Url string 		`json:"url"`
-	Secret string	`json:"secret"`
+	Url    string `json:"url"`
+	Secret string `json:"secret"`
 }
 
 func readConfig(file string) config {
-    // Open our jsonFile
-    jsonFile, err := os.Open(file)
-    // if we os.Open returns an error then handle it
-    if err != nil {
-        fmt.Println(err)
-    }
-    // defer the closing of our jsonFile so that we can parse it later on
-    defer jsonFile.Close()
+	// Open our jsonFile
+	jsonFile, err := os.Open(file)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
 	// read our opened jsonFile as a byte array.
-    byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 	// we initialize config
 	var conf config
 	// we unmarshal our byteArray which contains our jsonFile's content into conf
-    json.Unmarshal([]byte(byteValue), &conf)
+	json.Unmarshal([]byte(byteValue), &conf)
 
 	return conf
 }
 
 func main() {
 
-	conf := readConfig("config.json");
+	conf := readConfig("config.json")
 
 	bbbapi, err := api.NewRequest(conf.Url, conf.Secret, api.SHA256)
 	if err != nil {
 		panic(err)
 	}
 
+	newmeeting, err := bbbapi.CreateMeeting("name", "meetingID", "attendeePW", "moderatorPW", "welcome text", false, false, false, 12345)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("New meeting \"%s\" was created.\n", newmeeting.MeetingName)
+
 	meetings, err := bbbapi.GetMeetings()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("All meetings:")
 	for _, meeting := range meetings {
 		fmt.Println(meeting.MeetingName)
 	}
