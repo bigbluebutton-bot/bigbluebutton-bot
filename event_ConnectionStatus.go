2713a0821eda578083b8af804ae20bfc4c05affd
stjukrop <julian.kropp@stud.h-da.de>
2023-01-12 10:33:21 +0100

    :arrow_up: Update ddp: Status DALLING

--- event_ConnectionStatus.go
+++ event_ConnectionStatus.go
@@ -6,9 +6,10 @@ import (
 )
 
 type statusListener func(StatusType)
+
 // OnStatus in order to receive status changes.
 func (c *Client) OnStatus(listener statusListener) {
-	if(c.events["OnStatus"] == nil) {
+	if c.events["OnStatus"] == nil {
 		e := &event{
 			client: c,
 		}
@@ -18,23 +19,24 @@ func (c *Client) OnStatus(listener statusListener) {
 	c.events["OnStatus"] = append(c.events["OnStatus"], listener)
 }
 
-
 // Will be emited by ddpClient
 func (e *event) Status(status int) {
 	var st StatusType
 	switch status {
-		case ddp.CONNECTING:
-			st = CONNECTING
-		case ddp.CONNECTED:
-			st = CONNECTED
-		case ddp.DISCONNECTING:
-			st = DISCONNECTING
-		case ddp.DISCONNECTED:
-			st = DISCONNECTED
-		case ddp.RECONNECTING:
-			st = RECONNECTING
-		default:
-			st = DISCONNECTED
+	case ddp.DALLING:
+		st = CONNECTING
+	case ddp.CONNECTING:
+		st = CONNECTING
+	case ddp.CONNECTED:
+		st = CONNECTED
+	case ddp.DISCONNECTING:
+		st = DISCONNECTING
+	case ddp.DISCONNECTED:
+		st = DISCONNECTED
+	case ddp.RECONNECTING:
+		st = RECONNECTING
+	default:
+		st = DISCONNECTED
 	}
 
 	e.client.updateStatus(st)
@@ -50,9 +52,9 @@ func (c *Client) updateStatus(status StatusType) {
 
 		// call event(status)
 		f := reflect.TypeOf(event)
-		if(f.Kind() == reflect.Func) { //is function
-			if(f.NumIn() == 1 && f.NumOut() == 0) { //inbound parameters == 1, outbound parameters == 0
-				if(f.In(0).Kind() == reflect.String) { //parameter 0 is of type string (StatusType)
+		if f.Kind() == reflect.Func { //is function
+			if f.NumIn() == 1 && f.NumOut() == 0 { //inbound parameters == 1, outbound parameters == 0
+				if f.In(0).Kind() == reflect.String { //parameter 0 is of type string (StatusType)
 					reflect.ValueOf(event).Call([]reflect.Value{reflect.ValueOf(status)})
 				}
 			}
