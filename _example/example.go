ac159fc34cc4085963a2e6e67323aa36edfe30ac
JK <62835694+JulianKropp@users.noreply.github.com>
2023-10-10 15:27:39 +0200

    🧑‍💻

--- _example/example.go
+++ _example/example.go
@@ -18,86 +18,6 @@ import (
 	"github.com/pion/webrtc/v4/pkg/media/oggwriter"
 )
 
-type configAPI struct {
-	URL    string  `json:"url"`
-	Secret string  `json:"secret"`
-	SHA    api.SHA `json:"sha"`
-}
-
-type configClient struct {
-	URL string `json:"url"`
-	WS  string `json:"ws"`
-}
-
-type configPad struct {
-	URL string `json:"url"`
-	WS  string `json:"ws"`
-}
-
-type configWebRTC struct {
-	WS string `json:"ws"`
-}
-
-type configBBB struct {
-	API    configAPI	`json:"api"`
-	Client configClient	`json:"client"`
-	Pad configPad		`json:"pad"`
-	WebRTC configWebRTC `json:"webrtc"`
-}
-
-type config struct {
-	BBB configBBB `json:"bbb"`
-}
-
-func readConfig(file string) config {
-	// Try to read from env
-	conf := config {
-		BBB: configBBB{
-			API: configAPI{
-				URL: os.Getenv("BBB_API_URL"),
-				Secret: os.Getenv("BBB_API_SECRET"),
-				SHA: api.SHA(os.Getenv("BBB_API_SHA")),
-			},
-			Client: configClient{
-				URL: os.Getenv("BBB_CLIENT_URL"),
-				WS: os.Getenv("BBB_CLIENT_WS"),
-			},
-			Pad: configPad{
-				URL: os.Getenv("BBB_PAD_URL"),
-				WS: os.Getenv("BBB_PAD_WS"),
-			},
-			WebRTC: configWebRTC{
-				WS: os.Getenv("BBB_WEBRTC_WS"),
-			},
-		},
-	}
-
-	if (conf.BBB.API.URL != "" && conf.BBB.API.Secret != "" && conf.BBB.API.SHA != "" &&
-		conf.BBB.Client.URL != "" && conf.BBB.Client.WS != "" &&
-		conf.BBB.Pad.URL != "" && conf.BBB.Pad.WS != ""){
-		fmt.Println("Using env variables for config")
-		return conf
-	}
-
-	// Open our jsonFile
-	jsonFile, err := os.Open(file)
-	// if we os.Open returns an error then handle it
-	if (err != nil) {
-		fmt.Println(err)
-	}
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-	// read our opened jsonFile as a byte array.
-	byteValue, err := io.ReadAll(jsonFile)
-	if(err != nil) {
-		panic(err)
-	}
-	// we unmarshal our byteArray which contains our jsonFile's content into conf
-	json.Unmarshal([]byte(byteValue), &conf) 
-
-	return conf
-}
-
 func main() {
 
 	conf := readConfig("config.json")
@@ -291,4 +211,93 @@ func main() {
 	for _, meeting := range meetings {
 		fmt.Println(meeting.MeetingName)
 	}
+}
+
+
+
+
+
+
+
+
+
+
+type configAPI struct {
+	URL    string  `json:"url"`
+	Secret string  `json:"secret"`
+	SHA    api.SHA `json:"sha"`
+}
+
+type configClient struct {
+	URL string `json:"url"`
+	WS  string `json:"ws"`
+}
+
+type configPad struct {
+	URL string `json:"url"`
+	WS  string `json:"ws"`
+}
+
+type configWebRTC struct {
+	WS string `json:"ws"`
+}
+
+type configBBB struct {
+	API    configAPI	`json:"api"`
+	Client configClient	`json:"client"`
+	Pad configPad		`json:"pad"`
+	WebRTC configWebRTC `json:"webrtc"`
+}
+
+type config struct {
+	BBB configBBB `json:"bbb"`
+}
+
+func readConfig(file string) config {
+	// Try to read from env
+	conf := config {
+		BBB: configBBB{
+			API: configAPI{
+				URL: os.Getenv("BBB_API_URL"),
+				Secret: os.Getenv("BBB_API_SECRET"),
+				SHA: api.SHA(os.Getenv("BBB_API_SHA")),
+			},
+			Client: configClient{
+				URL: os.Getenv("BBB_CLIENT_URL"),
+				WS: os.Getenv("BBB_CLIENT_WS"),
+			},
+			Pad: configPad{
+				URL: os.Getenv("BBB_PAD_URL"),
+				WS: os.Getenv("BBB_PAD_WS"),
+			},
+			WebRTC: configWebRTC{
+				WS: os.Getenv("BBB_WEBRTC_WS"),
+			},
+		},
+	}
+
+	if (conf.BBB.API.URL != "" && conf.BBB.API.Secret != "" && conf.BBB.API.SHA != "" &&
+		conf.BBB.Client.URL != "" && conf.BBB.Client.WS != "" &&
+		conf.BBB.Pad.URL != "" && conf.BBB.Pad.WS != ""){
+		fmt.Println("Using env variables for config")
+		return conf
+	}
+
+	// Open our jsonFile
+	jsonFile, err := os.Open(file)
+	// if we os.Open returns an error then handle it
+	if (err != nil) {
+		fmt.Println(err)
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	// read our opened jsonFile as a byte array.
+	byteValue, err := io.ReadAll(jsonFile)
+	if(err != nil) {
+		panic(err)
+	}
+	// we unmarshal our byteArray which contains our jsonFile's content into conf
+	json.Unmarshal([]byte(byteValue), &conf) 
+
+	return conf
 }
